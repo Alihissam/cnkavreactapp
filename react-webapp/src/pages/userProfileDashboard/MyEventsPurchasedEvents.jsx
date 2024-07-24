fa8bca64553ad33ebf7de654e549bafadf7dec23
zohaibalishah <zohaibalishah1997@gmail.com>
2024-07-24 20:49:58 +0500

    dropdon

--- react-webapp/src/pages/userProfileDashboard/MyEventsPurchasedEvents.jsx
+++ react-webapp/src/pages/userProfileDashboard/MyEventsPurchasedEvents.jsx
@@ -5,32 +5,36 @@ const MyEventsPurchasedEvents = () => {
     <div className="flex flex-col justify-center items-center min-h-screen py-8 bg-gray-900">
       <div className="w-full max-w-lg mt-16">
         <form className="space-y-4">
-          <input
-            type="text"
-            id="searchChat"
+          <select
+            id="eventType"
             className="bg-white border-gray-700 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
-            placeholder="Select Event Type"
-          />
-          <input
-            type="text"
-            id="search"
-            className="bg-white border border-gray-700 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
-            placeholder="Search"
-          />
+          >
+            <option value="" disabled selected>Select Event Type</option>
+            <option value="conference">Conference</option>
+            <option value="workshop">Workshop</option>
+            <option value="webinar">Webinar</option>
+            <option value="meetup">Meetup</option>
+          </select>
+          <div className="relative">
+            <input
+              type="text"
+              id="search"
+              className="bg-white border border-gray-700 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 pr-10"
+              placeholder="Search"
+            />
+            <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+              <svg className="h-5 w-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M12.9 14.32a8 8 0 111.414-1.414l4.387 4.387a1 1 0 01-1.414 1.414l-4.387-4.387zM8 14a6 6 0 100-12 6 6 0 000 12z" clipRule="evenodd" />
+              </svg>
+            </div>
+          </div>
           <input
             type="text"
             id="loadChat"
             className="bg-white border border-gray-700 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3"
             placeholder="Post New Community Event"
           />
-          <div className="mt-20 flex justify-center">
-            <input
-              type="text"
-              id="loadChat"
-              className="bg-white mt-10 border border-gray-700 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-3/4 p-3"
-              placeholder="Load Events here from the Selection"
-            />
-          </div>
+        
         </form>
 
         <div className="flex justify-center">
